023d796b71e3ce012a8d8d3e9f062f02d4396e7e
Ainazik Kamaldinova <60198958+Ainazik02@users.noreply.github.com>
2022-03-19 01:48:28 -0500

    Update ViewController.swift

--- Flashcards/Flashcards/ViewController.swift
+++ Flashcards/Flashcards/ViewController.swift
@@ -7,7 +7,11 @@
 
 import UIKit
 
+struct Flashcard {
+    var question: String
+    var answer: String}
 class ViewController: UIViewController {
+    
 
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
@@ -18,6 +22,22 @@ class ViewController: UIViewController {
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         questionLabel.isHidden=true;
+    }
+    
+    func updateFlashcard(question: String, answer:String){
+
+
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // We know the destination of segue is the Navigation Controller
+        let navigationController = segue.destination as! UINavigationController
+        
+        // We know the Navigation Controller only contains a Creation View Controller
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        // We set the flashcardsController prop to self
+        creationController.flashcardsController = self
+
     }
     
 }
