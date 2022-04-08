9fa35f141090f8e231647aa3d2b01566b19db51f
Ainazik Kamaldinova <60198958+Ainazik02@users.noreply.github.com>
2022-04-08 17:21:19 -0500

    Update ViewController.swift

--- Flashcards/Flashcards/ViewController.swift
+++ Flashcards/Flashcards/ViewController.swift
@@ -14,6 +14,7 @@ struct Flashcard{
 class ViewController: UIViewController {
     
 
+    @IBOutlet weak var card: UIView!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var nextButton: UIButton!
@@ -23,7 +24,14 @@ class ViewController: UIViewController {
     var currentIndex=0
     
     override func viewDidLoad() {
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        questionLabel.clipsToBounds = true
+        answerLabel.clipsToBounds = true
+
         super.viewDidLoad()
+        //UserDefaults.standard.removeObject(forKey: "flashcards")
         readSavedFlashcards()
         if flashcards.count == 0{
             updateFlashcard(question: "What is the smallest bird in the world?", answer: "Hummingbird")}
@@ -34,19 +42,39 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        questionLabel.isHidden = !questionLabel.isHidden
+        flipFlashcard()
     }
+    func flipFlashcard(){
+        UIView.transition(with: card, duration: 0.3, options: .transitionFlipFromRight, animations : { [self] in
+            self.questionLabel.isHidden = !self.questionLabel.isHidden})
+}
+
     
     @IBAction func didTapOnNext(_ sender: Any) {
         currentIndex = currentIndex + 1
         updateLables()
         updateNextPrevButtons()
+        animateCardOut()
     }
     
     @IBAction func didTapOnPrev(_ sender: Any) {
         currentIndex = currentIndex - 1
         updateLables()
         updateNextPrevButtons()
+    }
+    func animateCardOut(){
+        UIView.animate(withDuration: 0.3, animations: {self.card.transform=CGAffineTransform.identity.translatedBy(x: -300.0, y: 0.0)}, completion: { finished in
+            self.updateLables()
+            self.animateCardIn()
+            
+        })
+    }
+    func animateCardIn(){
+        card.transform=CGAffineTransform.identity.translatedBy(x: 300.0, y: 0.0)
+        UIView.animate(withDuration:0.3){
+            self.card.transform=CGAffineTransform.identity
+        }
+    
     }
     
     
